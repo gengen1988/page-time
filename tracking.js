a2a219a249063e7643c4a7412334083f362ef466
Liu Gen <lg@e-teng.com.cn>
2018-10-23 10:10:05 +0800

    remove jquery, rewrite with es5

--- tracking.js
+++ tracking.js
@@ -1,17 +1,21 @@
-function tracking(tag = 'tracker', margin = 50) {
-  const socket = io()
-  const areas = $(`.${tag}`)
-    .map((i, el) => ({
-      cls: $(el).attr('class').split(' ').filter(v => v != tag).join(),
-      top: $(el).offset().top,
-      bottom: $(el).offset().top + $(el).height()
-    }))
-    .toArray()
+function tracking(tag, margin) {
+  tag = tag || 'tracker'
+  margin = margin || 50
+
+  var socket = io()
+  var areas = Array.prototype.map.call(document.querySelectorAll('.' + tag), function (el) {
+    return {
+      cls: el.className.split(' ').filter(function (v) {return v != tag}).join(),
+      top: el.offsetTop,
+      bottom: el.offsetTop + el.offsetHeight
+    }
+  })
 
   function areasOnScreen(areas, margin) {
-    const scrollTop = $(window).scrollTop()
-    const windowHeight = $(window).height()
-    return areas.reduce((memo, area) => {
+    var scrollTop = window.scrollY
+    var windowHeight = window.innerHeight
+
+    return areas.reduce(function (memo, area) {
       if (scrollTop > area.top - windowHeight + margin && scrollTop < area.bottom - margin) {
         memo.push(area.cls)
       }
